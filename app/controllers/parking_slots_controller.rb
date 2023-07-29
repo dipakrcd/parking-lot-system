c4fe6588617fe198f95470a118331114a2522d92
dipakrcd <dipakchaudhari153@gmail.com>
2023-07-29 11:26:14 +0530

    display messages added

--- app/controllers/parking_slots_controller.rb
+++ app/controllers/parking_slots_controller.rb
@@ -1,31 +1,38 @@
 class ParkingSlotsController < ApplicationController
-    skip_before_action :verify_authenticity_token
-    def index 
-      @parking_slots = ParkingSlot.all
+  skip_before_action :verify_authenticity_token
+
+  def index 
+    @parking_slots = ParkingSlot.all
+  end
+
+  def park_car
+    car_registration_number = params[:registration_number]
+    car_color = params[:color]
+    parking_slot_id = find_nearest_distance
+    car = Car.new(registration_number: car_registration_number, color: car_color)
+    if car.save
+      if parking_slot_id
+        parking_slot = ParkingSlot.find(parking_slot_id)
+        car_parked_at_parking_slot = parking_slot.update(is_empty: true,car_id: car.id) 
+        flash[:notice] = "Car Parked"
+        redirect_to parking_slots_path
+      else
+        flash[:alert] = "No space avaibale to park car"
+        redirect_to parking_slots_path
+      end
+    else
+      flash[:error] = car.errors.full_messages
+      redirect_to parking_slots_path
     end
-    def park_car
-        car_registration_number = params[:registration_number]
-        car_color = params[:color]
-        parking_slot_id = find_nearest_distance
-        car = Car.new(registration_number: car_registration_number, color: car_color)
-        if car.save
-          if parking_slot_id
-              parking_slot = ParkingSlot.find(parking_slot_id)
-              car_parked_at_parking_slot = parking_slot.update(is_empty: true,car_id: car.id)
-              redirect_to parking_slots_path
-          else
-              render json: { message: "no slot available for parking" }, status: :not_found 
-          end
-        else
-             render json: { errors: car.errors.full_messages }, status: :unprocessable_entity
-        end
+  end
+
+  def find_nearest_distance
+    parking_slot = ParkingSlot.where(is_empty:false).order(:distance_from_entry_point).first
+    if parking_slot
+      return parking_slot.id
+    else 
+      return nil
     end
-    def find_nearest_distance
-        parking_slot = ParkingSlot.where(is_empty:false).order(:distance_from_entry_point).limit(1)
-        if parking_slot
-          return parking_slot[0][:id]
-        else 
-          return nil
-        end
-    end 
+  end 
+
 end
