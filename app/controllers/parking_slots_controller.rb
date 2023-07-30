e7a337947ef7534b1de27a688421ca255f88503b
dipakrcd <dipakchaudhari153@gmail.com>
2023-07-30 10:24:19 +0530

    parking lot view updated

--- app/controllers/parking_slots_controller.rb
+++ app/controllers/parking_slots_controller.rb
@@ -8,20 +8,27 @@ class ParkingSlotsController < ApplicationController
     car_registration_number = params[:registration_number]
     car = Car.find_by(registration_number: car_registration_number)
     parking_slot_id = find_nearest_distance
-    if !is_parked?(car)
-      if parking_slot_id
-        ticket = Ticket.create(car_id: car.id)
-        parking_slot = ParkingSlot.find(parking_slot_id)
-        car_parked_at_parking_slot = parking_slot.update(is_empty: true,ticket_id: ticket.id)
-        flash[:notice] = "Car Parked"
-        redirect_to parking_slots_path
+
+    if car.nil?
+      flash[:alert] = "Car not found"
+      redirect_to root_path
+    else
+          
+      if !is_parked?(car)
+        if parking_slot_id
+          ticket = Ticket.create(car_id: car.id)
+          parking_slot = ParkingSlot.find(parking_slot_id)
+          car_parked_at_parking_slot = parking_slot.update(is_empty: true,ticket_id: ticket.id)
+          flash[:notice] = "Car Parked"
+          redirect_to parking_slots_path
+        else
+          flash[:alert] = "No space avaibale to park car"
+          redirect_to parking_slots_path
+        end
       else
-        flash[:alert] = "No space avaibale to park car"
+        flash[:alert] = "Car is already parked"
         redirect_to parking_slots_path
       end
-    else
-      flash[:alert] = "Car is already parked"
-      redirect_to parking_slots_path
     end
   end
 
@@ -38,6 +45,7 @@ class ParkingSlotsController < ApplicationController
     car_registration_number = params[:registration_number]
     car = Car.find_by(registration_number: car_registration_number)
     ticket = car.ticket.where(is_car_parked: true).first
+    
     if car.present? && ticket.present?
       ticket.update(is_car_parked: false)
       ticket.parking_slot.update(is_empty: false,ticket_id: nil)
@@ -51,6 +59,7 @@ class ParkingSlotsController < ApplicationController
 
   def is_parked?(car)
     ticket = Ticket.where(car_id: car.id,is_car_parked: true)
+    
     if ticket.present?
       return true
     else
