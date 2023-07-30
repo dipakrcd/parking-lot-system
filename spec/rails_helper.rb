99518ea1feffcf57caaa5603eccf00fa2acdd453
dipakrcd <dipakchaudhari153@gmail.com>
2023-07-30 16:27:54 +0530

    rspec test cases added for controller and model

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'factory_bot_rails'
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+end
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
